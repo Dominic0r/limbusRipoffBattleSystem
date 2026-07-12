a4965ca543fdc815a43589b95f8715c82edbe4c9
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 09:44:32 +0800

    Create Coin.java

---
+++ Coin.java
@@ -0,0 +1,42 @@
+package com.dominic0r.limbusripoff;
+import java.util.*;
+public class Coin{
+        Random ra = new Random();
+        private Consumer<clashResult> onHitEffect;
+        int atkPoints; // attack points
+        
+        String description;
+        
+        public Coin(int atkPoints, String description){
+            this(atkPoints, description, rst ->{});
+        }
+        
+        public Coin (int atkPoints, String description, Consumer<clashResult> onHitEffect){
+            this.atkPoints = atkPoints;
+            this.description = description;
+            this.onHitEffect = onHitEffect;
+        }
+        
+        public void triggerOnHit(clashResult result){
+            if(onHitEffect !=null){
+                onHitEffect.accept(result);
+            }
+        }
+        
+        public String getDesc(){ return description;}
+        public int getAtkPoints() { return atkPoints;}
+        
+        public boolean coinToss(int morale){
+            int tresh = 50 + morale;
+            return ra.nextInt(100)< tresh;
+        }
+        
+        public int getCoinPower(int morale){
+            int tresh = 50+morale;
+            if(ra.nextInt(100)< tresh){
+                return atkPoints;
+            }else{
+                return 0;
+            }
+        }
+    }
