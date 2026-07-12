c27fc865729b8658d6fe83b5091bcb99a370797c
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 16:40:38 +0800

    Create MoveCondition.java

---
+++ combat/MoveCondition.java
@@ -0,0 +1,12 @@
+package combat;
+
+@FunctionalInterface
+public interface MoveCondition {
+    /**
+     * @param field The active battlefield state.
+     * @param user The unit attempting to use this move.
+     * @param target The unit being targeted (can be null if checking general availability).
+     * @return true if the move can be selected/executed; false otherwise.
+     */
+    boolean canUse(Battlefield field, Unit user, Unit target);
+}
