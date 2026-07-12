ae9dc0a2bc6fbae5e2c6073e100e1024a35b17a2
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 09:45:36 +0800

    Create Move.java

---
+++ Move.java
@@ -0,0 +1,38 @@
+package com.dominic0r.limbusripoff;
+import java.util.*;
+
+public static class Move{ 
+        String name;
+        String description;
+        List <Coin> coinSet = new ArrayList<>();
+        int baseatk;
+        
+        public Move(String name, int baseatk, String description){
+            this.name = name;
+            this.description = description;
+            this.baseatk = baseatk;
+        }
+        
+        public String getName(){return name;}
+        public String getDesc(){ return description;}
+        public int getBaseAtk(){ return baseatk;}
+        
+        public int getTotalPoints(){
+            int tot = baseatk;
+            for(Coin co: coinSet){
+                tot+= co.getAtkPoints();
+            }
+            return tot;
+        }
+        
+        public void addCoin(Coin toAdd){
+            coinSet.add(toAdd);
+        }
+        
+        public List<Coin> getCoinSet(){ return coinSet;}
+        
+        
+        public List<Coin> getClashCoins(){ // USE THIS FOR BATTLES
+            return new ArrayList<>(this.coinSet);
+        }
+}
