9d1fa1493bd85f41cd79bfc0f1b86bb3fb7c1f04
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 22:04:12 +0800

    Create limbusPassives.java

---
+++ content/limbusPassives.java
@@ -0,0 +1,131 @@
+// Not 100% accurate but I'll try!
+package content;
+
+import java.util.*;
+
+import combat.*;
+import effect.*;
+import registry.*;
+
+
+public class BaseContent implements ContentPackage{
+    @Override
+    public String getPackageId(){
+        return "limbus_passives";
+    }
+
+    @Override
+  public void registerContent(){
+    statusEffect bleed = new statusEffect("Bleed", false, 99, "Take fixed damage every coin toss");
+        bleed.setOnClash((field, un)->{
+            int damagetaken=0;
+            for(appliedEffect app : un.getEffectList()){
+                if(app.stat() == bleed){
+                    damagetaken = app.getPotency();
+                    un.takeHPDamage(damagetaken);
+                    app.decrementStack();
+                    System.out.println("  > " + un.getName() + " took " + damagetaken + " bleed damage!");
+                }
+            }
+        })
+          .setOnHitGive((field,un)->{
+            int damagetaken=0;
+            for(appliedEffect app : un.getEffectList()){
+                if(app.stat() == bleed){
+                    damagetaken = app.getPotency();
+                    un.takeHPDamage(damagetaken);
+                    app.decrementStack();
+                    System.out.println("  > " + un.getName() + " took " + damagetaken + " bleed damage!"); 
+                }
+            }
+          });
+    Registry.registerStatus("lim:bleed", bleed);
+
+    statusEffect rupture = new statusEffect("Rupture", false, 99, "Take fixed damage every hit");
+        rupture.setOnHitReceived((field, un)->{
+            int damagetaken=0;
+            for(appliedEffect app : un.getEffectList()){
+                if(app.stat() == rupture){
+                    damagetaken = app.getPotency();
+                    un.takeHPDamage(damagetaken);
+                    app.decrementStack();
+                    System.out.println("  > " + un.getName() + " took " + damagetaken + " rupture damage!"); 
+                }
+            }
+        });
+    Registry.registerStatus("lim:rupture", rupture);
+
+    statusEffect burn = new statusEffect("Burn", false, 99, "Take fixed damage every end of turn");
+        burn.setOnTurnEnd((field, un)->{
+            int damagetaken=0;
+            for(appliedEffect app : un.getEffectList()){
+                if(app.stat() == burn){
+                    damagetaken = app.getPotency();
+                    un.takeHPDamage(damagetaken);
+                    app.decrementStack();
+                    System.out.println("  > " + un.getName() + " took " + damagetaken + " burn damage!"); 
+                }
+            }
+        });
+    Registry.registerStatus("lim:burn", burn);
+
+    statusEffect sinking = new statusEffect("Sinking", false, 99, "Take fixed Morale damage every hit");
+        sinking.setOnHitReceived((field, un)->{
+            int damagetaken=0;
+            for(appliedEffect app : un.getEffectList()){
+                if(app.stat() == sinking){
+                    damagetaken = app.getPotency();
+                    un.takeMoraleDamage(damagetaken);
+                    app.decrementStack();
+                    System.out.println("  > " + un.getName() + " took " + damagetaken + " sinking damage!"); 
+                }
+            }
+        });
+    Registry.registerStatus("lim:sinking", sinking);
+
+    statusEffect poise = new statusEffect("Poise", false, 99, "+5% chance of critical hit on target");
+        poise.setOnTurnStart((field, un)->{
+            int critChanceModify=0;
+            for(appliedEffect app : un.getEffectList()){
+                if(app.stat() == poise){
+                    critChanceModify += app.getPotency()*5;
+                    un.modifyCritChance(critChanceModify);
+                    app.decrementStack();
+                    System.out.println("  > " + un.getName() + "'s crit chance is increased by " + critChanceModify + "%!"); 
+                }
+            }
+        });
+    Registry.registerStatus("lim:poise", poise);
+
+    statusEffect charge = new statusEffect("Charge", false, 99, "Can be used by skills");
+        
+    Registry.registerStatus("lim:charge", charge);
+
+    statusEffect tremor = new statusEffect("Tremor", false, 99, "Increases stagger threshold upon tremor burst");
+    tremor.setOnTurnEnd((field,un)->{
+      for(appliedEffect app : un.getEffectList()){
+                if(app.stat() == tremor){
+                    app.decrementStack();
+                }
+            }
+    });
+    Registry.registerStatus("lim:tremor", tremor);
+
+    statusEffect tremorBurst = new statusEffect("Tremor Burst", false, 1, "increases stagger treshold by the amount of tremor potency");
+    tremorBurst.setOnTurnEnd((field,un)->{
+      for(appliedEffect app : un.getEffectList()){
+        if(app.stat()==tremorBurst){
+          int tremorAmt = 0;
+          for(appliedEffect findTrem: un.getEffectList()){
+            if(findTrem.stat() == tremor){
+              tremorAmt = findTrem.getPotency();
+            }
+          }
+          un.modifyStaggerTresh(tremorAmt);
+        }
+      }
+    });
+    
+  }
+
+}
