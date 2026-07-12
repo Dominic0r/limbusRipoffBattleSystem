5609c6cd6994dde8d54ab635d2d6d435b78aa368
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 09:40:57 +0800

    Create mutation.java

---
+++ mutation.java
@@ -0,0 +1,22 @@
+package com.dominic0r.limbusripoff;
+
+public static class mutation{
+        
+        final Type type;
+        final int amount;
+        final statusEffect effect;
+        final Unit source;
+        
+        public mutation(Type type, int amount, statusEffect effect, Unit source){
+            this.type = type;
+            this.amount = amount;
+            this.effect = effect;
+            this.source = source;
+        }
+        
+        public Type getType(){return type;}
+        public int getAmount(){ return amount;}
+        public statusEffect getEffect(){ return effect;}
+        public Unit getSource(){ return source;}
+        
+}
