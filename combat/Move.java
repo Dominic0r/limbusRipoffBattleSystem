b271a54bf156464abfdd7d0dbc18b4de52a9f1e9
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 16:45:22 +0800

    Update Move.java

--- combat/Move.java
+++ combat/Move.java
@@ -7,11 +7,24 @@ public class Move{
         String description;
         List <Coin> coinSet = new ArrayList<>();
         int baseatk;
+
+        private MoveCondition condition = (field, user, target) -> true;
         
         public Move(String name, int baseatk, String description){
             this.name = name;
             this.description = description;
             this.baseatk = baseatk;
+        }
+
+        public Move setCondition(MoveCondition condition) {
+                if (condition != null) {
+                    this.condition = condition;
+                }
+                return this; 
+        }
+
+        public boolean isUsable(Battlefield field, Unit user, Unit target) {
+                return condition.canUse(field, user, target);
         }
         
         public String getName(){return name;}
