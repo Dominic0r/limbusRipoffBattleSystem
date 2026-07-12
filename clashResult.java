eb661733f3cb6fa1ef9284c0d241f46f336cd5f3
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 09:52:44 +0800

    Create clashResult.java

---
+++ clashResult.java
@@ -0,0 +1,20 @@
+package com.dominic0r.limbusripoff;
+
+public class clashResult{
+        Unit winner;
+        Unit loser;
+        int remainingCoins;
+        List<Coin> winnerCoinSet = new ArrayList<>();
+        
+        public clashResult(Unit winner, Unit loser, int remainingCoins, List<Coin> winnerCoinSet){
+            this.winner = winner;
+            this.loser = loser;
+            this.remainingCoins = remainingCoins;
+            this.winnerCoinSet = winnerCoinSet;
+        }
+        
+        public Unit getWinner(){ return winner;}
+        public Unit getLoser(){ return loser;}
+        public int getRemCoins() { return remainingCoins;}
+        public List<Coin> getCoinSet(){ return winnerCoinSet;}
+    }
