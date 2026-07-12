8d2271859f12bdeaf300639555c01529fbc5c867
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 09:51:17 +0800

    Create combatContext.java

---
+++ combatContext.java
@@ -0,0 +1,25 @@
+package com.dominic0r.limbusripoff;
+
+public class combatContext{
+        Unit attacker;
+        Move attackerMove;
+        Unit defender;
+        Move defenderMove;
+        int totalDamage;
+        Battlefield field;
+        
+        public combatContext (Unit attacker, Move attackerMove, Unit defender, Move defenderMove, Battlefield field){
+            this.attacker = attacker;
+            this.attackerMove = attackerMove;
+            this.defender = defender;
+            this.defenderMove = defenderMove;
+            this.field = field;
+        }
+        public Unit getAttacker(){ return attacker;}
+        public Move getAttackerMove(){ return attackerMove;}
+        public Unit getDefender(){ return defender;}
+        public Move getDefenderMove(){ return defenderMove;}
+        public int totalDamage(){ return totalDamage;}
+        public Battlefield field() {return field;}
+        
+    }
