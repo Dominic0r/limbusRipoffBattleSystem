13989d96547f7f5f5a2ad9fdd1eeb52462de7df0
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 22:14:36 +0800

    Update BaseContent.java

--- content/BaseContent.java
+++ content/BaseContent.java
@@ -92,8 +92,8 @@ public class BaseContent implements ContentPackage{
         playerMoveSet.add(roundhouse);
         playerMoveSet.add(stab);
         playerMoveSet.add(nuke);
-        
-        Unit playerUnit = new Unit(100, 0, 5, 30, "Player", "Description", playerMoveSet);
+        //public Unit(int hp, int morale, int speed, int staggerTresh, int critChance, float critmodifier, String name, String description, List<Move> moveSet){
+        Unit playerUnit = new Unit(100, 0, 5, 30, 2,0.2, "Player", "Description", playerMoveSet);
         
         Registry.registerUnit("base:player", playerUnit);
         
@@ -112,7 +112,7 @@ public class BaseContent implements ContentPackage{
         
         defEnemyMoveset.add(punch);
         
-        Unit defEnemy = new Unit(50, 0, 2, 15, "Enemy", "Default Enemy", defEnemyMoveset);
+        Unit defEnemy = new Unit(50, 0, 2, 15, 2,0.2,  "Enemy", "Default Enemy", defEnemyMoveset);
         
         Registry.registerUnit("base:enemy", defEnemy);
         
@@ -122,7 +122,7 @@ public class BaseContent implements ContentPackage{
         
         defAllyMoveSet.add(punch);
         
-        Unit defAlly = new Unit(50, 0, 2, 15, "Ally", "Default Ally", defAllyMoveSet);
+        Unit defAlly = new Unit(50, 0, 2, 15, 2,0.2,  "Ally", "Default Ally", defAllyMoveSet);
         Registry.registerUnit("base:ally", defAlly);
     }
 }
