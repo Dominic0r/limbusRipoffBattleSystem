e4ba66a75f990f5ff000fffa50fc7546516458f0
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-12 09:37:28 +0800

    Create statusEffect.java

---
+++ statusEffect.java
@@ -0,0 +1,70 @@
+package com.dominic0r.limbusripoff;
+public class statusEffect{
+        String name, description;
+        
+        boolean decays; // whether or not it decays every turn 
+        int limit;
+        
+        private BiConsumer<Battlefield, Unit> onTurnStart;
+        private BiConsumer<Battlefield, Unit> onHitReceived;
+        private BiConsumer<Battlefield, Unit> onTurnEnd;
+        private BiConsumer<Battlefield, Unit> onClash;
+        private BiConsumer<Battlefield, Unit> onHitGive;
+        
+        public statusEffect(String name, boolean decays, int limit, String description){
+            this.name = name;
+            this.description = description;
+            this.decays = decays;
+            this.limit = limit;
+        }
+        
+        public statusEffect setOnTurnStart(BiConsumer<Battlefield, Unit> hook) {
+            this.onTurnStart = hook;
+            return this;
+        }
+
+        public statusEffect setOnHitReceived(BiConsumer<Battlefield, Unit> hook) {
+            this.onHitReceived = hook;
+            return this;
+        }
+    
+        public statusEffect setOnTurnEnd(BiConsumer<Battlefield, Unit> hook) {
+            this.onTurnEnd = hook;
+            return this;
+        }
+        
+        public statusEffect setOnClash(BiConsumer<Battlefield, Unit> hook) {
+            this.onClash = hook;
+            return this;
+        }
+        
+        public statusEffect setOnHitGive(BiConsumer<Battlefield, Unit> hook) {
+            this.onHitGive = hook;
+            return this;
+        }
+        
+        public void triggerTurnStart(Battlefield field, Unit un) {
+            if (onTurnStart != null) onTurnStart.accept(field, un);
+        }
+    
+        public void triggerOnHitReceived(Battlefield field, Unit un) {
+            if (onHitReceived != null) onHitReceived.accept(field, un);
+        }
+    
+        public void triggerTurnEnd(Battlefield field, Unit un) {
+            if (onTurnEnd != null) onTurnEnd.accept(field, un);
+        }
+        
+        public void triggerOnClash(Battlefield field, Unit un){
+            if(onClash != null) onClash.accept(field, un);
+        }
+        
+        public void triggerOnHitGive(Battlefield field, Unit un){
+            if(onHitGive != null) onHitGive.accept(field, un);
+        }
+        
+        public String getName(){ return name;}
+        public String getDesc(){ return description;}
+        public boolean Decays() { return decays;}
+        public int getLimit() { return limit;}
+    }
