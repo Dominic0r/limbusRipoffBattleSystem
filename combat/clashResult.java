3b69f9573fd36c97ed393f723926d2537ba643f2
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-13 22:25:39 +0800

    Update clashResult.java

--- combat/clashResult.java
+++ combat/clashResult.java
@@ -7,15 +7,24 @@ public class clashResult{
         int remainingCoins;
         List<Coin> winnerCoinSet = new ArrayList<>();
         
-        public clashResult(Unit winner, Unit loser, int remainingCoins, List<Coin> winnerCoinSet){
+        List<Coin> loserUnbreakables = new ArrayList<>();
+        
+        public clashResult(Unit winner, Unit loser, int remainingCoins, List<Coin> winnerCoinSet, List<Coin> loserUnbreakables, List<Coin> winnerUnbreakables){
             this.winner = winner;
             this.loser = loser;
             this.remainingCoins = remainingCoins;
             this.winnerCoinSet = winnerCoinSet;
+                this.loserUnbreakables = loserUnbreakables;
+                for(Coin co: winnerUnbreakables){
+                        if(!winnerCoinSet.contains(co)){
+                                winnerCoinSet.add(co);
+                        }
+                }
         }
         
         public Unit getWinner(){ return winner;}
         public Unit getLoser(){ return loser;}
         public int getRemCoins() { return remainingCoins;}
         public List<Coin> getCoinSet(){ return winnerCoinSet;}
+        public List<Coin> getLoserUnbreakables(){return loserUnbreakables;}
     }
