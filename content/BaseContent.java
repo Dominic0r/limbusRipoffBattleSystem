007720bec150449c4f9eb6215c0b672c372963ba
Dominic0r <106385363+Dominic0r@users.noreply.github.com>
2026-07-13 19:18:06 +0800

    Update BaseContent.java

--- content/BaseContent.java
+++ content/BaseContent.java
@@ -22,29 +22,29 @@ public class BaseContent implements ContentPackage{
         
         Move multiPunch = new Move("Multi-Punch", 5, "Punches the Enemy 3 times");
         multiPunch.addCoin(new Coin(2, "Punch!", rst ->{
-            rst.getLoser().takeHPDamage(2, rst.getWinner());
+            rst.getLoser().takeHPDamage(2, rst.getWinner(), damageType.BLUNT);
         }));
         
         multiPunch.addCoin(new Coin(1, "Punch Again!", rst ->{
-            rst.getLoser().takeHPDamage(1,rst.getWinner());
+            rst.getLoser().takeHPDamage(1,rst.getWinner(), damageType.BLUNT);
         }));
         
         multiPunch.addCoin(new Coin(3, "Upper Cut!", rst ->{
-            rst.getLoser().takeHPDamage(3,rst.getWinner());
+            rst.getLoser().takeHPDamage(3,rst.getWinner(),damageType.BLUNT);
         }));
         
         Move roundhouse = new Move("Roundhouse Kick", 3, "Kicks the enemy hard");
         roundhouse.addCoin(new Coin(10, "Kick!", rst ->{
-            rst.getLoser().takeHPDamage(10,rst.getWinner());
+            rst.getLoser().takeHPDamage(10,rst.getWinner(),damageType.PIERCE);
         }));
         
         Move stab = new Move("Stab", 4, "Stabs the enemy twice");
         stab.addCoin(new Coin(3, "Swish!", rst ->{
-            rst.getLoser().takeHPDamage(3,rst.getWinner());
+            rst.getLoser().takeHPDamage(3,rst.getWinner(),damageType.SLASH);
         }));
         
         stab.addCoin(new Coin(3, "Slash! - Inflicts 3 bleed potency", rst ->{
-            rst.getLoser().takeHPDamage(3,rst.getWinner());
+            rst.getLoser().takeHPDamage(3,rst.getWinner(),damageType.SLASH);
             if(!rst.getLoser().getEffectList().isEmpty()){
                 for(appliedEffect app: rst.getLoser().getEffectList()){
                     if(app.stat()==bleed){
@@ -72,15 +72,15 @@ public class BaseContent implements ContentPackage{
             });
 
         nuke.addCoin(new Coin(10, "NUKE!", rst->{
-            rst.getLoser().takeHPDamage(100,rst.getWinner());
+            rst.getLoser().takeHPDamage(100,rst.getWinner(),damageType.BLUNT);
         }));
         
         playerMoveSet.add(multiPunch);
         playerMoveSet.add(roundhouse);
         playerMoveSet.add(stab);
         playerMoveSet.add(nuke);
-        //public Unit(int hp, int morale, int speed, int staggerTresh, int critChance, int baseAtk, int baseDef, float critmodifier, String name, String description, List<Move> moveSet){
-        Unit playerUnit = new Unit(100, 0, 5, 30, 2, 10, 10,0.2f, "Player", "Description", playerMoveSet);
+        //public Unit(int hp, int morale, int speed, int staggerTresh, int critChance, int baseAtk, int baseDef, float critmodifier, float slash, float pierce, float blunt, String name, String description, List<Move> moveSet){
+        Unit playerUnit = new Unit(100, 0, 5, 30, 2, 10, 10,0.2f,0.5f,0.5f,1.0f, "Player", "Description", playerMoveSet);
         
         Registry.registerUnit("base:player", playerUnit);
         
@@ -91,15 +91,15 @@ public class BaseContent implements ContentPackage{
         
         punch.addCoin(new Coin(1, "punch-", rst ->{
             
-            rst.getLoser().takeHPDamage(1,rst.getWinner());
+            rst.getLoser().takeHPDamage(1,rst.getWinner(),damageType.BLUNT);
         }));
         punch.addCoin(new Coin(1, "punch again-", rst ->{
-            rst.getLoser().takeHPDamage(1,rst.getWinner());
+            rst.getLoser().takeHPDamage(1,rst.getWinner(),damageType.BLUNT);
         }));
         
         defEnemyMoveset.add(punch);
         
-        Unit defEnemy = new Unit(50, 0, 2, 30, 2,8,8,0.2f,  "Enemy", "Default Enemy", defEnemyMoveset);
+        Unit defEnemy = new Unit(50, 0, 2, 30, 2,8,8,0.2f,1.5f,1.0f,0.5f,  "Enemy", "Default Enemy", defEnemyMoveset);
         
         Registry.registerUnit("base:enemy", defEnemy);
         
@@ -109,7 +109,7 @@ public class BaseContent implements ContentPackage{
         
         defAllyMoveSet.add(punch);
         
-        Unit defAlly = new Unit(50, 0, 2, 15, 2,8,8,0.2f,  "Ally", "Default Ally", defAllyMoveSet);
+        Unit defAlly = new Unit(50, 0, 2, 15, 2,8,8,0.2f,1.5f,1.0f,0.5f,  "Ally", "Default Ally", defAllyMoveSet);
         Registry.registerUnit("base:ally", defAlly);
     }
 }
